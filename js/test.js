d8c893659ce18f7c6855b69a6a662616fbcd547c
khaled079k <khaledaz079@gmail.com>
2020-09-21 19:33:11 +0100

    abbas.cv

---
+++ js/test.js
@@ -0,0 +1,9 @@
+function opennav(){
+	document.getElementById('mynav').style.width="50%";
+	document.getElementById('main').style.opacity="0";
+}
+
+function closenav(){
+	document.getElementById('mynav').style.width="00%";
+	document.getElementById('main').style.opacity="1";
+}
